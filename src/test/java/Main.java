75c4776bca81416250c644e2b747575f0118a1a4
Stefan <stefan.marian2511@gmail.com>
2025-11-15 11:25:52 +0200

    Third Commit

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,85 +1,60 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main(String[] args) throws InterruptedException {
 
+    public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         driver.manage().window().maximize();
 
-        Thread.sleep(2000);
+        clearThenType(wait, By.name("username"), "Admin");
+        clearThenType(wait, By.name("password"), "admin123");
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']"))).click();
 
-        driver.findElement(By.name("username")).sendKeys("Admin");
-        driver.findElement(By.name("password")).sendKeys("admin123");
-        driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='PIM']"))).click();
 
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//span[text()=\"PIM\"]")).click();
+        clearThenType(wait, By.xpath("//label[text()='Employee Name']/../following-sibling::div//input"), "Amelia");
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']"))).click();
 
-        Thread.sleep(2000);
-        WebElement nameInput = driver.findElement(By.xpath("//label[text()=\"Employee Name\"]/../following-sibling::div//input"));
-        nameInput.sendKeys("Amelia");
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@role='cell'][3]/div"))).click();
 
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@role='tab'][2]/a"))).click();
 
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//div[@role=\"cell\"][3]/div")).click();
+        clearThenType(wait, By.xpath("//label[text()='Street 1']/../following-sibling::div/input"), "firstStreet");
+        clearThenType(wait, By.xpath("//label[text()='Street 2']/../following-sibling::div/input"), "secondStreet");
+        clearThenType(wait, By.xpath("//label[text()='City']/../following-sibling::div/input"), "randomCity");
+        clearThenType(wait, By.xpath("//label[text()='State/Province']/../following-sibling::div/input"), "randomState");
+        clearThenType(wait, By.xpath("//label[text()='Zip/Postal Code']/../following-sibling::div/input"), "00000");
 
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//div[@role=\"tab\"][2]/a")).click();
+        clearThenType(wait, By.xpath("//label[text()='Home']/../following-sibling::div/input"), "0733333333");
+        clearThenType(wait, By.xpath("//label[text()='Mobile']/../following-sibling::div/input"), "0744444444");
+        clearThenType(wait, By.xpath("//label[text()='Work']/../following-sibling::div/input"), "0755555555");
 
-        Thread.sleep(1000);
-        WebElement firstStreet = driver.findElement(By.xpath("//label[text()=\"Street 1\"]/../following-sibling :: div/input"));
-        firstStreet.clear();
-        firstStreet.sendKeys("First Street");
+        clearThenType(wait, By.xpath("//label[text()='Work Email']/../following-sibling::div/input"), "workemail@work.com");
+        clearThenType(wait, By.xpath("//label[text()='Other Email']/../following-sibling::div/input"), "otheremail@other.com");
 
-        WebElement secondStreet = driver.findElement(By.xpath("//label[text()=\"Street 2\"]/../following-sibling :: div/input"));
-        secondStreet.sendKeys("Second Street");
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div//button[@type='submit']"))).click();
 
-        WebElement city = driver.findElement(By.xpath("//label[text()=\"City\"]/../following-sibling :: div/input"));
-        city.sendKeys("randomCity");
+    }
 
-        WebElement state = driver.findElement(By.xpath("//label[text()=\"State/Province\"]/../following-sibling :: div/input"));
-        state.sendKeys("randomState");
+    private static void clearThenType(WebDriverWait wait, By locator, String text) {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        element.click();
 
-        WebElement postalCode = driver.findElement(By.xpath("//label[text()=\"Zip/Postal Code\"]/../following-sibling :: div/input"));
-        postalCode.sendKeys("00000");
+        while (!element.getAttribute("value").isEmpty()) {
+            element.sendKeys(Keys.BACK_SPACE);
+        }
 
-//        driver.findElement(By.xpath("//div[@class=\"oxd-select-wrapper\"]//i")).click();
-
-//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
-//        WebElement romania = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[6]/div/div[2]/div/div[1]/div[2]/i")));
-//        romania.click();
-
-        WebElement homePhone = driver.findElement(By.xpath("//label[text()=\"Home\"]/../following-sibling :: div/input"));
-        homePhone.sendKeys("0733333333");
-
-        WebElement mobilePhone = driver.findElement(By.xpath("//label[text()=\"Mobile\"]/../following-sibling :: div/input"));
-        mobilePhone.sendKeys("0744444444");
-
-        WebElement workPhone = driver.findElement(By.xpath("//label[text()=\"Work\"]/../following-sibling :: div/input"));
-        workPhone.sendKeys("0755555555");
-
-        WebElement workEmail = driver.findElement(By.xpath("//label[text()=\"Work Email\"]/../following-sibling :: div/input"));
-        workEmail.sendKeys("workemail@work.com");
-
-        WebElement otherEmail = driver.findElement(By.xpath("//label[text()=\"Other Email\"]/../following-sibling :: div/input"));
-        otherEmail.sendKeys("otheremail@other.com");
-
-        Thread.sleep(3000);
-        driver.findElement(By.xpath("//div//button[@type=\"submit\"]")).click();
-
+        element.sendKeys(text);
     }
 }
 
